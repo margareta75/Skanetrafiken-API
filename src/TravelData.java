8c00703c8b2c25dde5e5e65efed79f6e72ec9aa3
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-01-29 11:13:40 +0100

    Travel data handling added

---
+++ src/TravelData.java
@@ -0,0 +1,38 @@
+
+
+import java.util.ArrayList;
+
+public class TravelData {
+	
+	private String from;
+	private String to;
+	private ArrayList<Journey> journeys = new ArrayList<Journey>();
+	
+	public String getFrom() {
+		return from;
+	}
+	
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public ArrayList<Journey> getJourneys() {
+		return journeys;
+	}
+	
+	public void addJourney(Journey journey) {
+		this.journeys.add(journey);
+	}
+
+
+	
+
+}
