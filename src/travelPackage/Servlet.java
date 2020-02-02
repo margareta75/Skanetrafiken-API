018b4cf684feb0d3e37637b08bb4607b6c56d472
Chainreaktion <Johan@edenfjord.se>
2020-02-02 10:11:38 +0100

    Så, klarat det mesta nu.
    Det är inte den snyggaste koden Men är lite stolt ändå! :)

--- src/travelPackage/Servlet.java
+++ src/travelPackage/Servlet.java
@@ -25,11 +25,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
-//import com.sun.tools.javac.code.Type.ForAll;
-
-//import com.sun.tools.sjavac.comp.dependencies.PublicApiCollector;
-//import sun.security.action.GetBooleanAction;
-
 /**
  * Servlet implementation class Servlet
  */
@@ -56,25 +51,23 @@ public class Servlet extends HttpServlet {
 		String rawTo;
 		String from = "";
 		String to = "";
-		
+
 		request.setCharacterEncoding("UTF-8");
-		
+
 		rawFrom = request.getParameter("current");
 		rawTo = request.getParameter("to");
-		System.out.println(rawFrom + " " + rawTo);
-
+		
 		if (rawFrom != null) {
 			from = URLEncoder.encode(rawFrom, "UTF-8");
 		}
-		
+
 		if (rawTo != null) {
 			to = URLEncoder.encode(rawTo, "UTF-8");
 		}
+
 		
-		System.out.println(from + " " + to);
-		
-		// Build the API call by adding city+country into a URL
-		String URLtoSend = "http://labs.skanetrafiken.se/v2.2/querystation.asp?inpPointFr=" + from + "&inpPointTo=" + to;
+		// Building the Api so we can search the start point!
+		String URLtoSend = "http://labs.skanetrafiken.se/v2.2/querystation.asp?inpPointFr=" + from;
 
 		System.out.println(URLtoSend);
 
@@ -99,7 +92,6 @@ public class Servlet extends HttpServlet {
 		// loop through the whole response
 		while ((inputLine = in.readLine()) != null) {
 
-			// System.out.println(inputLine);
 			// Save the temp line into the full response
 			ApiResponse += inputLine;
 		}
@@ -116,30 +108,59 @@ public class Servlet extends HttpServlet {
 
 		// Create a Node list that gets everything in and under Startpoints!
 		NodeList nList = doc.getElementsByTagName("StartPoints");
-//		NodeList startList;
-		
-		System.out.println(nList);
-		
-//		if (nList.getLength() > 0) {
-//			startList = nList.item(0).getChildNodes();			
-//		}
-		
-//		NodeList nodeList = doc.getElementsByTagName("EndPoints");
-//		NodeList endList;
-//		
-//		if (nodeList.getLength() > 0) {
-//			endList = nodeList.item(0).getChildNodes();			
-//		}
-//		
-		
-		int startID = checkNode(nList, from);
-		int endID = 78114; // checkNode(endList, to);
+
+		int startID = idFetch(nList, rawFrom); // fetches the ID  for the startStation
+
+		// Building an apifor the To search as well! :)
+		URLtoSend = "http://labs.skanetrafiken.se/v2.2/querystation.asp?inpPointFr=" + to;
+
+		System.out.println(URLtoSend);
+
+		// Set the URL that will be sent
+		line_api_url = new URL(URLtoSend);
+
+		// Create a HTTP connection to sent the GET request over
+		linec = (HttpURLConnection) line_api_url.openConnection();
+		linec.setDoInput(true);
+		linec.setDoOutput(true);
+		linec.setRequestMethod("GET");
+
+		// Make a Buffer to read the response from the API
+		in = new BufferedReader(new InputStreamReader(linec.getInputStream(), "UTF-8"));
+
+		// Emptying the apiResponse string for the loop downunder!
+		ApiResponse = "";
+
+		// loop through the whole response
+		while ((inputLine = in.readLine()) != null) {
+
+			// Save the temp line into the full response
+			ApiResponse += inputLine;
+		}
+		in.close(); // closing in. because its best to do it
+		System.out.println(ApiResponse);
+
+		// Call the method to make a XMLdoc out of the full response
+		doc = convertStringToXMLDocument(ApiResponse);
+
+		// normalize the XML response
+		doc.getDocumentElement().normalize();
+
+		// check that the XML response is OK by getting the Root element
+		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+		// Create a Node list that gets everything in and under Startpoints!
+		NodeList dList = doc.getElementsByTagName("StartPoints");
+
+		int endID = idFetch(dList, rawTo);
+
+		// Making an array to store the two Ids that is needed for the next API response
 
 		int travelIDs[] = { startID, endID };
 
 		request.setAttribute("travelID", travelIDs);
 
-		// Skickar datan vidare till traveldataservletten!
+		// Sending the saved id Data to the Travelservlet
 		RequestDispatcher idSender = request.getRequestDispatcher("TravelDataServlet");
 		idSender.forward(request, response);
 
@@ -175,30 +196,29 @@ public class Servlet extends HttpServlet {
 		return null;
 	}
 
-	private int checkNode(NodeList node, String name) {
-		
-		int idNumber;
-		for(int i = 0; i < node.getLength(); i++ ) {	//går i genom alla points en efter en tills man hittar rätt point!
-			
-			NodeList startList = node.item(i).getChildNodes();			
-			
-			
-			for (int j = 0; j < startList.getLength(); j++) { //Ska hittar rätt ID och name
-				if(startList.item(j).getNodeName().equals("ID")) {
+	private int idFetch(NodeList node, String name) {
+		NodeList nList = node.item(0).getChildNodes();
+		int idNumber = 0;
+		for (int i = 0; i < nList.getLength(); i++) { // går i genom alla points en efter en tills man hittar rätt
+														// point!
+
+			NodeList pointNode = nList.item(i).getChildNodes();
+
+			for (int j = 0; j < pointNode.getLength(); j++) { // Ska hittar rätt ID och name
+				if (pointNode.item(j).getNodeName().equals("Id")) {
 					try {
-					idNumber = Integer.parseInt(startList.item(j).getTextContent());
-					}catch(NumberFormatException e) {
+						idNumber = Integer.parseInt(pointNode.item(j).getTextContent());
+					} catch (NumberFormatException e) {
 						idNumber = 0;
 					}
 				}
-				
-				if(startList.item(j).getTextContent().equals(name)) {
-				System.out.println(startList.item(j).getTextContent());
-				return idNumber;
+
+				if (pointNode.item(j).getTextContent().equals(name)) {
+					return idNumber;
 				}
-	
+
 			}
-		
+
 		}
 		return idNumber;
 	}
